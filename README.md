c99aa0d418231c563f22412667423cb8a417d9cf
Andrea Mariani <ieo5776@ieo.it>
2024-08-26 16:26:27 +0200

    creating garmin repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# Garmin Data Unraveled
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+Some analysis of my own data. Ranging from sleep, to performance to bedtime and so on.  
+
+## Sleep Data
+
+In this notebook i'll try to dive deep into my sleep data. Some questions i might try to answer are:
+1) Do i sleep consistently? Any bias towards certain months?
+2) How good or bad my sleep is? 
+3) Can i predict sleep score solely based on night performance? Is it weigheted somehow?
---
+++ utils/sleep_functions.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+
+def plt_sleep(daily_sleep):
+	sns.set(font_scale = 1)
+	sns.set_style(style='white')
+
+	plt.figure(figsize=(10, 6))
+
+	#sns.lineplot(data=daily_sleep, legend=False, palette=["#E69F00"])
+	sns.scatterplot(data=daily_sleep, legend=False, palette=["#56B4E9"])
+
+	plt.ylabel(None)
+	plt.xlabel(None)
+	plt.tick_params(bottom=True, left=True)
+	plt.title("Daily Sleep Quality over the last 19 months")
+	sns.despine(offset=10, trim=True)
+
+	ax = plt.gca()
+	ax.xaxis.set_major_locator(mdates.MonthLocator())
+	ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
+
+	plt.xticks(rotation=45, ha="right")
+
+	plt.show()
+
+# Convert time to hours, with 21:00 as the starting point
+def convert_time(time_obj):
+    hours_from_21 = time_obj.hour + time_obj.minute / 60 - 21
+    return hours_from_21 if hours_from_21 >= 0 else hours_from_21 + 24
+
+def plot_bedtime(df):
+	plt.figure(figsize=(10, 6))
+	sns.scatterplot(x=df.index, y=df["sleep_start_hours"], label="Sleep Start",color=["#E69F00"])
+	sns.scatterplot(x=df.index, y=df["sleep_end_hours"], label="Sleep End", color=["#56B4E9"])
+
+	# Formatting
+	plt.title("Sleep Start and End Time in the last 19 months")
+	plt.ylabel("Time of Day")
+	plt.xlabel("Date")
+	plt.xticks(df.index[::4], rotation=45, ha="right")
+	plt.yticks(range(0, 24, 2), [f"{(h + 21) % 24:02d}:00" for h in range(0, 24, 2)])
+	plt.ylim(0, 18)
+	plt.tight_layout()
+	plt.legend()
+	plt.show()
